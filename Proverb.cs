e02b58fa5d6453df3745953cbf56e5c993227e5e
Cihlář Martin <Martin.Cihlar@ysoft.com>
2022-04-06 09:32:11 +0200

    Add project files.

---
+++ Proverb.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Proverb
+{
+    public static string[] Recite(string[] subjects) => ReciteStream(subjects).ToArray();
+
+    private static IEnumerable<string> ReciteStream(IEnumerable<string> subjects)
+    {
+        var firstSubject = "";
+        while(subjects.Any())
+        {
+            if (firstSubject == "")
+            {
+                firstSubject = subjects.First();
+            }
+
+            var currentSubjects = subjects.Take(2).ToArray();
+            switch (currentSubjects.Count())
+            {
+                case 2:
+                    yield return $"For want of a {currentSubjects.First()} the {currentSubjects.Last()} was lost.";
+                    break;
+                case 1:
+                    yield return $"And all for the want of a {firstSubject}.";
+                    break;
+                case 0:
+                    yield break;
+            } 
+            subjects = subjects.Skip(1);
+        }
+        
+    }
+}
